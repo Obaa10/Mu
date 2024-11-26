4b0e215894d45f3d29292d25febe66ac3c738e0f
Obaa10 <obada.maleh@gmail.com>
2024-11-26 12:35:03 +0200

    Update app module

--- apps/mu/src/app/app.module.ts
+++ apps/mu/src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { AuthModule } from './auth/auth.module';
-import { CoModule } from './s/co/co.module';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from '@mu/database/index';
+import { ConfigModule } from '@nestjs/config';
+import { HelperRedisModule } from '@mu/redis/*';
 
 @Module({
-  imports: [UserModule, CoModule, AuthModule, PostModule],
+  imports: [
+    UserModule, AuthModule, PostModule, DatabaseModule, HelperRedisModule, ConfigModule.forRoot({
+      isGlobal: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
