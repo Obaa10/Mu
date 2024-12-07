9fff97c1624cfe100d44a8eca695019b0ab383cb
Obaa10 <obada.maleh@gmail.com>
2024-12-07 13:24:51 +0200

    Update post module

--- apps/mu/src/app/post/post.module.ts
+++ apps/mu/src/app/post/post.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { UserEntity } from '@mu/database/lib/entities/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReactionEntity } from '@mu/database/lib/entities/reaction.entity';
+import { PostEntity } from '@mu/database/lib/entities/post.entity';
+import { CommentEntity } from '@mu/database/lib/entities/comment.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([PostEntity, CommentEntity, ReactionEntity]),
+  ],
   controllers: [PostController],
   providers: [PostService],
 })
-export class PostModule {}
+export class PostModule { }
