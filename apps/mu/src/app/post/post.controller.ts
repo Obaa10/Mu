d36576a893c0dc7f024c1e691dba3e367d24355e
Obaa10 <obada.maleh@gmail.com>
2024-12-07 13:24:37 +0200

    Update post controller, add new endpoints.

--- apps/mu/src/app/post/post.controller.ts
+++ apps/mu/src/app/post/post.controller.ts
@@ -1,34 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdatePostDto } from './dto/update-post.dto';
+import { JwtAuthGuard } from '../auth/strategy/jwt.guard';
+import { JwtPayload } from '../auth/dto/jwt-payload';
+import { User } from '../auth/decorators/session.decorator';
+import { AddCommentDto } from './dto/add-comment.dto';
+import { AddReactionDto } from './dto/add-reaction.dto';
 
 @Controller('post')
+@UseGuards(JwtAuthGuard)
 export class PostController {
-  constructor(private readonly postService: PostService) {}
+    constructor(private readonly postService: PostService) {}
 
-  @Post()
-  create(@Body() createPostDto: CreatePostDto) {
-    return this.postService.create(createPostDto);
-  }
+    @Post()
+    createPost(@Body() createPostDto: CreatePostDto, @User() user: JwtPayload) {
+        return this.postService.createPost(createPostDto, user.userId);
+    }
 
-  @Get()
-  findAll() {
-    return this.postService.findAll();
-  }
+    @Get(':id')
+    getPostById(@Param('id') id: number) {
+        return this.postService.getPostById(id);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.postService.findOne(+id);
-  }
+    @Post(':id/comment')
+    addComment(
+        @Param('id') postId: number,
+        @Body() addCommentDto: AddCommentDto,
+        @User() user: JwtPayload
+    ) {
+        return this.postService.addComment(postId, addCommentDto, user.userId);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
-    return this.postService.update(+id, updatePostDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.postService.remove(+id);
-  }
+    @Post(':id/reaction')
+    addReaction(
+        @Param('id') postId: number,
+        @Body() addReactionDto: AddReactionDto,
+        @User() user: JwtPayload
+    ) {
+        return this.postService.addReaction(postId, addReactionDto, user.userId);
+    }
 }
